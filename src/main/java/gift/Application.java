102323577fb583b0b788078db0b6bc94b323b46c
HyunJunCho00 <catlove5478@naver.com>
2025-07-25 08:36:22 -0700

    refactor: 애플리케이션 구성 및 업데이트

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -1,9 +1,11 @@
 package gift;
 
+import gift.config.kakao.KakaoProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@EnableConfigurationProperties(KakaoProperties.class)
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
