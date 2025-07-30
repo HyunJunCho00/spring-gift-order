5dd40a2c4804c03b9093b5cb72554710df50fa31
HyunJunCho00 <catlove5478@naver.com>
2025-07-30 22:03:17 +0900

    feat: 카카오 로그인 기능 구현 인증 로직

---
+++ src/main/java/gift/dto/kakao/KakaoMessageDto.java
@@ -0,0 +1,37 @@
+package gift.dto.kakao;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gift.entity.Order;
+
+public record KakaoMessageDto(
+        @JsonProperty("object_type") String objectType,
+        String text,
+        Link link,
+        @JsonProperty("button_title") String buttonTitle
+) {
+    public record Link(
+            @JsonProperty("web_url") String webUrl,
+            @JsonProperty("mobile_web_url") String mobileWebUrl
+    ) {}
+
+    public static String toTemplateObject(Order order, ObjectMapper objectMapper) throws JsonProcessingException {
+        String messageText = String.format(
+                "주문이 완료되었습니다.\\n\\n- 상품명: %s\\n- 옵션: %s\\n- 수량: %d개\\n- 메시지: %s",
+                order.getOption().getProduct().getName(),
+                order.getOption().getName(),
+                order.getQuantity(),
+                order.getMessage()
+        );
+        String productUrl = "http://localhost:8080/products/" + order.getOption().getProduct().getId();
+
+        var messageDto = new KakaoMessageDto(
+                "text",
+                messageText,
+                new Link(productUrl, productUrl),
+                "주문 상세 보기"
+        );
+        return objectMapper.writeValueAsString(messageDto);
+    }
+}
